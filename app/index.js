4947f770e30991b36cb722f710b9c656d7933b28
Sohaib Farooqi <sohaibfarooqi@hotmail.com>
2019-04-12 15:27:37 +0800

    application js entry point

---
+++ app/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+import configureStore from './configureStore'
+import HomePage from './containers/HomePage'
+
+const store = configureStore();
+const MOUNT_NODE = document.getElementById('app');
+
+ReactDOM.render(
+  <Provider store={store}>
+    <HomePage />
+  </Provider>,
+MOUNT_NODE);
