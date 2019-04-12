6648b83d8125668cac406f576db134fda0bd4adf
Sohaib Farooqi <sohaibfarooqi@hotmail.com>
2019-04-12 15:38:35 +0800

    doc string for tabview

--- app/components/TabView/index.js
+++ app/components/TabView/index.js
@@ -4,6 +4,12 @@ import PropTypes from 'prop-types'
 import { Tab } from 'react-tabs';
 import "react-tabs/style/react-tabs.css";
 
+/*
+Tabview component for application. This component
+is used inside `HomePage` container to display all
+possible resources of application i.e. `Entries`, `Blog`,
+`Author`, `Comment`
+*/
 class TabView extends React.Component{
 
   render(){
